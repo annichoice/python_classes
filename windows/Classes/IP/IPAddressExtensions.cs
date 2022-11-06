c10e0ce1262ca42a966b8c92860e3a7efc451d38
annichoice <123226437+annichoice@users.noreply.github.com>
2022-11-06 15:44:04 +0000

    remove settings

--- windows/Classes/IP/IPAddressExtensions.cs
+++ windows/Classes/IP/IPAddressExtensions.cs
@@ -9,4 +9,28 @@ namespace WinCFScan.Classes.IP
 {
     public static class IPAddressExtensions
     {
-        public static List<strin
+        public static List<string> getIPRange(string ip, int network)
+        {
+            uint start, end, total;
+            getIPRangeInfo(ip, network, out start, out end, out total);
+
+            var ipRange = new List<string>();
+
+            for (uint n = start; n < end; n++)
+            {
+                ipRange.Add(new IPAddress(BitConverter.GetBytes(n).Reverse().ToArray()).ToString());
+            }
+
+            return ipRange;
+
+        }
+
+        public static uint getIPRangeTotalIPs (string ipAndNet)
+        {
+            string[] splitted = ipAndNet.Split('/');
+            getIPRangeInfo(splitted[0], Int32.Parse(splitted[1]), out uint start, out uint end, out uint total);
+            return total;
+         }
+
+
+        public static void getIPRangeInfo(string ip, int network, 
