773da3ebe4b7f7e0147f1b833c94fb404198690d
annichoice <123226437+annichoice@users.noreply.github.com>
2022-12-02 06:25:22 +0000

    Update clean-blog.js

--- windows/Classes/ListListViewColumnSorter.cs
+++ windows/Classes/ListListViewColumnSorter.cs
@@ -27,4 +27,24 @@ namespace WinCFScan.Classes
         /// </summary>
         private CaseInsensitiveComparer ObjectCompare;
 
-        /// <summar
+        /// <summary>
+        /// Class constructor. Initializes various elements
+        /// </summary>
+        public ListViewColumnSorter()
+        {
+            // Initialize the column to '0'
+            ColumnToSort = 0;
+
+            // Initialize the sort order to 'none'
+            OrderOfSort = SortOrder.None;
+
+            // Initialize the CaseInsensitiveComparer object
+            ObjectCompare = new CaseInsensitiveComparer();
+        }
+
+        /// <summary>
+        /// This method is inherited from the IComparer interface. It compares the two objects passed using a case insensitive comparison.
+        /// </summary>
+        /// <param name="x">First object to be compared</param>
+        /// <param name="y">Second object to be compared</param>
+        /// <returns>The
